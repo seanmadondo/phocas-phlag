2e80f5b00dbbb7e4f72d6aacf7efb337d50ce94e
Sean Madondo <seanmadondo@yahoo.com>
2022-09-03 21:44:34 +1200

    contants

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const OPACITY_DURATION_MILLIS = 200;
+
+/* Link to Confluence Feature Flag Documentation */
+export const Phocas_Features_Link =
+  "https://helpphocassoftware.atlassian.net/wiki/spaces/DEV/pages/983663895/Feature+Flags?src=search";
+
+/* Link to Phlag Official Documentation */
+export const PhlagDocoLink =
+  "https://helpphocassoftware.atlassian.net/wiki/spaces/~752526820/pages/2857042010/Phlag+-+A+Feature+Flag+tool+for+Phocas";
